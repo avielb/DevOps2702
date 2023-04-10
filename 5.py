5776cff423a7711eac065e09331ad010b12978f5
Aviel Buskila <aviel33@gmail.com>
2023-04-10 19:29:35 +0300

    another commit (#1)

--- 5.py
+++ 5.py
@@ -1,11 +1,12 @@
 isTrue = False
-a = True
+a = False
 b = 2.5
 strOne = "One"
 strThree = "Three"
+aaa = "bbb"
 
 # check if it is a good day
-if a > b and (strOne == "aviel" or b > 1):
+if a > b and (strOne == "aviel" or b > 1): # if/else
     print("a is greater then b")
 elif a == b:
     print("a equals then b")
