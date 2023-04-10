a67ca0aa6e52d2f947f54681782f8a74af0d6705
Aviel Buskila <aviel33@gmail.com>
2023-04-10 19:38:06 +0300

    My new feature (#2)
    * another commit
    
    * another commit
    
    * another commit

--- 34.py
+++ 34.py
@@ -7,5 +7,16 @@ def check_if_url_up(url):
         return True
 
 
+def print_name(name):
+    print(name)
+
+
+def print_age(age):
+    print(age)
+
+
+def print_pet(pet):
+    print(pet)
+
 
 print(check_if_url_up("https://github.com"))
